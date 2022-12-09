32a7082d7161008e1a44fdd758b460604518c464
Ahmet Ateş <118804688+1Atesch@users.noreply.github.com>
2022-12-09 12:29:19 +0300

    Add files via upload
    ______________Oynanış______________
    1- Oyunun giriş ekranında, kaç tane tahmin edilecek sayı olmasını istediğimizi giriyoruz.
    2-Bir sonraki ekranda oyunu oynayacak oyuncu sayısını giriyoruz.
    3-Oyuncuların isimlerini sırasıyla yazıyoruz.
    4-Bu aşamada ismi yazan kullanıcı tahmin ettiği sayıyı giriyor.
    5-Bütün oyuncuların tahminlerinin tahmin edilmesi gereken sayıya ne kadar uzaklıkta olduğu hesaplanarak buna göre puan verilecektir.

---
+++ Ahmet Ateş/ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            while (true)
+            {
+                Random ates = new Random();
+                Console.WriteLine("Soru Sayısını Giriniz");
+                int sS = Convert.ToInt32(Console.ReadLine());
+                int[] random = new int[sS];
+                for (int i = 0; i < random.Length; i++)
+                {
+                    random[i] = ates.Next(0, 10);
+                    Console.WriteLine(random[i]);
+                }
+
+                Console.WriteLine("Oyuncu Sayısını Giriniz");
+                int oS = Convert.ToInt32(Console.ReadLine());
+                int[] puan = new int[oS];
+                string[] oyuncu = new string[oS];
+                for (int i = 0; i < oS; i++)
+                {
+
+                    puan[i] = 0;
+                    Console.Clear();
+
+                    Console.WriteLine($"{i + 1}. Oyuncunun İsmini Giriniz");
+                    oyuncu[i] = Console.ReadLine();
+                }
+                for (int i = 0; i < sS; i++)
+                {
+                    Console.Clear();
+                    for (int b = 0; b <= oS; b++)
+                    {
+
+                        Console.WriteLine($"Puanınız:{puan[b]}");
+                        Console.WriteLine($"Rastgele Sayi:{random[i]}");
+                        Console.WriteLine($" {oyuncu[b]} Tahminini Girsin");
+                        int tahmin = Convert.ToInt32(Console.ReadLine());
+                        if (random[i] == tahmin)
+                        {
+                            Console.WriteLine("Doğru Tahmin Ettiniz");
+                            puan[b] += 10;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            if (random[i] > tahmin)
+                            {
+                                Console.WriteLine("Yanlıs Tahmin Ettiniz");
+                                puan[i] += 10 - (random[i] - tahmin);
+                                Console.WriteLine(puan[b]);
+                                Console.Clear();
+                            }
+                            else if (random[i] < tahmin)
+                            {
+                                Console.WriteLine("Yanlıs Tahmin Ettiniz");
+                                puan[b] += 10 - (tahmin - random[i]);
+                                Console.WriteLine($"{puan[b]} puan eklendi");
+                                Console.Clear();
+                            }
+                        }
+
+                    }
+                }
+                for (int i = 0; i <= oS; i++)
+                {
+                    Console.Write($"{oyuncu[i]} Puanı={puan[i]}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
